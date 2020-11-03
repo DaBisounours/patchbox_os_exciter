38a9e0141b1dd1611d574d275ea9e806d439e375
Evan <evan.graine@gmail.com>
2020-11-03 19:58:45 +0000

    Update install

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ sudo apt-get install python3-dev python3-tk python3-pil.imagetk python3-pip
 git clone https://github.com/belangeo/pyo.git
 cd pyo
 sudo python3 setup.py install --use-jack --use-double
+
+pip3 install pyyaml
