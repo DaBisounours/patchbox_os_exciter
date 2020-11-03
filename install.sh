bed08d304b801fead3fe7ff7ead8812d28f1ce17
Evan <evan.graine@gmail.com>
2020-11-03 19:25:24 +0000

    Try compilling pyo from sources on install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 echo "Installing dependencies"
-pip3 install -U pyo
-
-echo "You're done!"
-
+sudo apt-get install libjack-jackd2-dev libportmidi-dev portaudio19-dev liblo-dev libsndfile-dev
+sudo apt-get install python3-dev python3-tk python3-pil.imagetk python3-pip
+git clone https://github.com/belangeo/pyo.git
+cd pyo
+sudo python3 setup.py install --use-jack --use-double
